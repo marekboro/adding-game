88ecde43cc7458b2521bd934579badd7a7e76eb0
Marek Borowicz <111274015+marekborowicz@users.noreply.github.com>
2023-07-22 19:39:21 +0100

    some formatting and added functionality

--- addingGame.py
+++ addingGame.py
@@ -1,38 +1,86 @@
+import sys
 import random
 import languageConverter as lc
 
 runAgain = True
 questionsAsked = 0
 equations = []
+argsProvided = False
+maxRounds: int
 
-def main():
-    runInfinite()
+
+def main(input: list[str]):
+    checkArgs(input)
+    runGames()
     logOutAllGames()
+
+
+def checkArgs(input: list[str]):
+    global argsProvided
+    global maxRounds
+    input.remove(input[0])
+    if int(input.__len__()) > 0:
+        argsProvided = True
+        try:
+            maxRounds = int(input[0])
+        except ValueError:
+            print("max runs for game cannot be a string")
+            sys.exit()
+        if maxRounds < 1:
+            print("max runs cannot be 0 or less")
+            sys.exit()
+
+
+def runGames():
+    global argsProvided
+    global maxRounds
+    if argsProvided:
+        if maxRounds == 1:
+            runOnce()
+        else:
+            runTimes(maxRounds)
+    else:
+        runInfinite()
+
+
+def runTimes(maxRounds: int):
+    global questionsAsked
+    global runAgain
+    while runAgain and questionsAsked < maxRounds:
+        if questionsAsked == 0:
+            runOnce()
+        choice = get_choice('play again? ')
+        if choice == "n" or choice == "no":
+            runAgain = False
+        else:
+            runOnce()
+
 
 def runOnce():
     global questionsAsked
-    number = generateRandomNumner()
+    number = generateRandomNumber()
     equation = generateEquationForNumber(number)
     # textEquation = generateTextEquationForNumber(number)
 
-    
-    runQuestion(equation,number)
+    runQuestion(equation, number)
     questionsAsked = questionsAsked+1
 
+
 def runOnceOld():
-    number = generateRandomNumner()
+    number = generateRandomNumber()
     equation = generateEquationForNumber(number)
     # textEquation = generateTextEquationForNumber(number)
-    runQuestion(equation,number)
+    runQuestion(equation, number)
+
 
 def runInfinite():
     global questionsAsked
     global runAgain
     while runAgain:
-        if questionsAsked==0:
+        if questionsAsked == 0:
             runOnce()
         choice = get_choice('play again? ')
-        if choice=="n" or choice=="no":
+        if choice == "n" or choice == "no":
             runAgain = False
         else:
             runOnce()
@@ -40,57 +88,71 @@ def runInfinite():
 
 def logOutAllGames():
     global equations
-    for index,equation in enumerate(equations):
+    for index, equation in enumerate(equations):
         print(f"{index+1} =>    {equation}")
+
 
 def runInfiniteOld():
     global runAgain
     while runAgain:
         choice = get_choice('play again? ')
-        if choice=="n" or choice=="no":
+        if choice == "n" or choice == "no":
             runAgain = False
         else:
             runOnce()
-      
-            
-def generateRandomNumner():
-    return random.randrange(16,35)
 
+
+def generateRandomNumber():
+    return random.randrange(16, 35)
+
+
+def generateRandomNumberMaxedAt(max: int):
+    if (max > 50):
+        print("He is only a child you monster, changing max to 40")
+        max = 40
+    min = round((0.25*max), 0)
+    return random.randrange(min, max)
 # def generateTextEquationForNumber(number):
 #     if number > 28:
 #         return createFourSegmentEquation(number)
 #     if number > 28:
 #         return createthreeSegmentEquation(number)
 #     return createTwoSegmentEquation(number)
-def generateEquationForNumber(number:int):
-    if(number>20):
-       return generateThreePartEquation(number)
+
+
+def generateEquationForNumber(number: int):
+    if (number > 20):
+        return generateThreePartEquation(number)
     return generateTwoPartEquation(number)
 
-def createFourSegmentEquation(number:int):
-    first = random.randrange(5,12)
-    second = random.randrange(5,number-first)
-    third = random.randrange(5,number-second-first)
+
+def createFourSegmentEquation(number: int):
+    first = random.randrange(5, 12)
+    second = random.randrange(5, number-first)
+    third = random.randrange(5, number-second-first)
     fourth = number-first-second-third
 
-def generateThreePartEquation(number:int):
-    first = random.randrange(5,12)
-    second = random.randrange(5,number-first)
+
+def generateThreePartEquation(number: int):
+    first = random.randrange(5, 12)
+    second = random.randrange(5, number-first)
     third = number-first-second
     return f"{first} + {second} + {third} = "
 
-def generateTwoPartEquation(number:int):
-    first = random.randrange(7,14)
+
+def generateTwoPartEquation(number: int):
+    first = random.randrange(7, 14)
     second = number-first
     return f"{first} + {second} = "
 
-def runQuestion(equarion:str,number:int):
+
+def runQuestion(equarion: str, number: int):
     global equations
-   
-    while(True):
+
+    while (True):
         try:
             result = get_int(equarion)
-            if result==number:
+            if result == number:
                 equations.append(f"{equarion}{result}")
                 print("Correct!")
                 return "next"
@@ -99,17 +161,28 @@ def runQuestion(equarion:str,number:int):
 
 
 def get_int(prompt):
-    while(True):
+    while (True):
         try:
+            # return int(checkExit(input(prompt)))
             return int(input(prompt))
         except ValueError:
             pass
 
+
+def checkExit(someString: str):
+    if someString == "q" or "quit":
+        sys.exit()
+    else:
+        return someString
+
+
 def get_choice(prompt):
-    while(True):
+    while (True):
         try:
             return input(prompt)
         except ValueError:
             pass
 
-main()
+
+if __name__ == "__main__":
+    main(sys.argv)
--- languageConverter.py
+++ languageConverter.py
@@ -1,64 +1,76 @@
-englishNumbers = {0:"Zero",1:"One",2:"Two",3:"Tree",4:"Four",5:"Five",6:"Six",7:"Seven",8:"Eight",9:"Nine",10:"Ten",11:"Eleven",12:"Twelve",13:"Thir-teen",14:"Four-teen",15:"Fif-teen",16:"Six-teen",17:"Seven-teen",18:"Eigh-teen",19:"Nine-teen",20:"Twenty",30:"Thirty",40:"Fourty",50:"Fifty",60:"Sixrty",70:"Seventy",80:"Eighty",90:"Ninety",100:"One Hundred",200:"Two Hundred",300:"Three Hundred",400:"Four Hundred",500:"Five Hundred",600:"Six Hundred",700:"Seven Hundred",800:"Eight Hundred",900:"Nine Hundred",}
-polishNumbers = {0:"Zero",1:"Jeden",2:"Dwa",3:"Trzy",4:"Cztery",5:"Pięć",6:"Sześć",7:"Siedem",8:"Osiem",9:"Dziewięć",10:"Dziesięć",11:"Jedenaście",12:"Dwanaście",13:"Trzynaście",14:"Czternaście",15:"Piętnaście",16:"Szesnaście",17:"Siedemnaście",18:"Osiemnaście",19:"Dziewiętnaście",20:"Dwadzieścia",30:"Trzydzieści",40:"Czterdzieści",50:"Pięćdziesiąt",60:"Sześćdziesiąt",70:"Siedemdziesiąt",80:"Osiemdziesiąt",90:"Dziewięćdziesiąt",100:"Sto",200:"Dwieście",300:"Trzysta", 400:"Czterysta",500:"Pięćset", 600: "Sześćset", 700:"Siedemset", 800:"Osiemset", 900:"Dziewięćset"}
-polishStatements = {"and":""}
-englishStatements = {"and":" and"}
+englishNumbers = {0: "Zero", 1: "One", 2: "Two", 3: "Tree", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine", 10: "Ten", 11: "Eleven", 12: "Twelve", 13: "Thir-teen", 14: "Four-teen", 15: "Fif-teen", 16: "Six-teen", 17: "Seven-teen", 18: "Eigh-teen", 19: "Nine-teen", 20: "Twenty",
+                  30: "Thirty", 40: "Fourty", 50: "Fifty", 60: "Sixrty", 70: "Seventy", 80: "Eighty", 90: "Ninety", 100: "One Hundred", 200: "Two Hundred", 300: "Three Hundred", 400: "Four Hundred", 500: "Five Hundred", 600: "Six Hundred", 700: "Seven Hundred", 800: "Eight Hundred", 900: "Nine Hundred", }
+polishNumbers = {0: "Zero", 1: "Jeden", 2: "Dwa", 3: "Trzy", 4: "Cztery", 5: "Pięć", 6: "Sześć", 7: "Siedem", 8: "Osiem", 9: "Dziewięć", 10: "Dziesięć", 11: "Jedenaście", 12: "Dwanaście", 13: "Trzynaście", 14: "Czternaście", 15: "Piętnaście", 16: "Szesnaście", 17: "Siedemnaście", 18: "Osiemnaście", 19: "Dziewiętnaście",
+                 20: "Dwadzieścia", 30: "Trzydzieści", 40: "Czterdzieści", 50: "Pięćdziesiąt", 60: "Sześćdziesiąt", 70: "Siedemdziesiąt", 80: "Osiemdziesiąt", 90: "Dziewięćdziesiąt", 100: "Sto", 200: "Dwieście", 300: "Trzysta", 400: "Czterysta", 500: "Pięćset", 600: "Sześćset", 700: "Siedemset", 800: "Osiemset", 900: "Dziewięćset"}
+polishStatements = {"and": ""}
+englishStatements = {"and": " and"}
 
 library = {
-    "Polish":{"numbers":polishNumbers,"statements":polishStatements},
-    "English":{"numbers":englishNumbers,"statements":englishStatements}}
+    "Polish": {"numbers": polishNumbers, "statements": polishStatements},
+    "English": {"numbers": englishNumbers, "statements": englishStatements}}
+
 
 def getLanguages():
     return list(library.keys())
 
+
 defaultLanguage = list(library.keys())[1]
 polish = list(library.keys())[0]
 
-def getLanguageStatements(language:str):
+
+def getLanguageStatements(language: str):
     return library[language]["statements"]
 
-# provides the language specific maping value for numbers up to 199
-def getNumbersForLanguage(language:str):
+# provides the language specific mapping value for numbers up to 199
+
+
+def getNumbersForLanguage(language: str):
     keys = library.keys()
     if keys.__contains__(language):
         return library[language]["numbers"]
-    else:   
+    else:
         return library["English"]["numbers"]
-    
-def convertIntToLanguage(number:int,language:str):
-    language = verifiedLanguage(language)
-    symbol, symbollessNumber = extractSymbolFromInt(number)
-    intLength = symbollessNumber.__str__().__len__()
 
-    if(symbollessNumber>999):
-        return(f"numbers above 999 not currently supperted in {getLanguages()}")
-    if(intLength==3):
-        return f"{symbol} {convert3DigitIntToLanguage(symbollessNumber,language)}"
-    else: 
-        return f"{symbol} {convert2DigitIntToLanguage(symbollessNumber,language)}"    
-    
-def extractSymbolFromInt(number:int):
+
+def convertIntToLanguage(number: int, language: str):
+    language = verifiedLanguage(language)
+    symbol, absoluteNumber = getSignAndAbsoluteOfInt(number)
+    intLength = absoluteNumber.__str__().__len__()
+
+    if (absoluteNumber > 999):
+        return (f"numbers above 999 not currently supperted in {getLanguages()}")
+    if (intLength == 3):
+        return f"{symbol} {convert3DigitIntToLanguage(absoluteNumber,language)}"
+    else:
+        return f"{symbol} {convert2DigitIntToLanguage(absoluteNumber,language)}"
+
+
+def getSignAndAbsoluteOfInt(number: int):
     symbol = ""
     stringNumber = number.__str__()
-    if stringNumber[0] == "-": 
+    if stringNumber[0] == "-":
         symbol = "Minus"
-        symbollessNumber = (int)(stringNumber.lstrip("-"))
-    return symbol, symbollessNumber
+        absoluteNumber = (int)(stringNumber.lstrip("-"))
+    return symbol, absoluteNumber
 
-def verifiedLanguage(languageRequested:str):
+
+def verifiedLanguage(languageRequested: str):
     if library.keys().__contains__(languageRequested):
         return languageRequested
     else:
-         defaultLanguage = "English"
-         print(f"{languageRequested} not supported, {defaultLanguage} used")
-         return defaultLanguage
+        defaultLanguage = "English"
+        print(f"{languageRequested} not supported, {defaultLanguage} used")
+        return defaultLanguage
 
-def convert3DigitIntToLanguage(number:int,language:str):
+
+def convert3DigitIntToLanguage(number: int, language: str):
     leadingDigit = (int)(number.__str__()[0])
     return f'{getNumbersForLanguage(language)[leadingDigit*100]}{getLanguageStatements(language)["and"]} {convert2DigitIntToLanguage(number-(100*leadingDigit),language)}'
 
-def convert2DigitIntToLanguage(number:int,language:str):
+
+def convert2DigitIntToLanguage(number: int, language: str):
     leadingDigit = (int)(number.__str__()[0])
-    if number>20:
+    if number > 20:
         return f"{getNumbersForLanguage(language)[10*leadingDigit]} {getNumbersForLanguage(language)[number-(10*leadingDigit)]}"
     else:
         return f"{getNumbersForLanguage(language)[number]}"
